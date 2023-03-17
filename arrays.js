62688d312d115c9884a9ea61357a93bf2a9493ae
linetmusembi <musembilinetmwongeli@gmail.com>
2023-03-17 19:43:22 +0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,59 @@
+// Find the last element of the following arrays.
+// arr1 = [3,7,34,90,12]
+// arr2 = [true, "green", "where",12,56]
+let arr1 = [3,7,34,90,12];
+let arr = arr1.slice(-1);
+console.log(arr);
+
+let arr2 = [true,"green","where",12,56];
+let arr3 = arr2.slice(-1);
+console.log(arr3)
+
+
+
+
+
+// write a JS program that will join the following array elements into a string
+// myPets = ["Cow", "Python", "Snake", "Dog"];
+const myPets = ["Cow","Python","Snake","Dog"];
+console.log(myPets.join());
+
+
+
+
+// write a JS script to sort the following array items
+// var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+var arra3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+arra3.sort();
+console.log(arra3);
+
+
+
+// Write a program to remove duplicates from the following array.
+// Console the array without duplicates, and console another array that only contains the duplicates
+// var arr = ["apple", "mango", "apple",
+//             "orange", "mango", "mango"];
+var arri = ["apple", "mango", "apple","orange", "mango", "mango"];
+
+
+
+
+
+
+
+// Write a JS script to search for the following word in the array.
+// 4
+// If the word is present, console it else console "the search word was not found"
+// let arr5 = ["the", "way", "x", 4, 23];
+let  arr5 = ["the", "way", "x", 4, 23];
+const plus = arr5.includes(4);
+console.log(plus)
+
+
+
+// Write a JS script to sort the following string
+// let word = "lufituaeb"
+ let word = "lufituaeb"
+ console.log(word.split("").sort().join(""));
+
+
